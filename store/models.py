f8554a6829dd9a529514493b129f754521f7a15b
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-24 12:10:25 +0300

    Made Category title to be unique

--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from store.validators import validate_file_size
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=155)
+    title = models.CharField(max_length=155, unique=True)
 
     def __str__(self):
         return self.title
