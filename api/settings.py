7f003bd5bdefedc45ab67ef00f53efe9313aba58
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-24 11:26:55 +0300

    Added settings to connect to MySQL database

--- api/settings.py
+++ api/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env("DB_NAME"),
+        'HOST': env("DB_HOST"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'PORT': env("DB_PORT")
     }
 }
 
