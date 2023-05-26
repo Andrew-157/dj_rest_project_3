226714963e04b273aff3ec8a73ca38402e59ae5f
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-26 10:20:21 +0300

    Applied filters to search for category, products, brand

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.permissions import SAFE_METHODS
 from rest_framework.exceptions import NotFound, MethodNotAllowed
 from rest_framework.response import Response
+from rest_framework import filters
 from store.permissions import IsAdminOrReadOnly
 from store.models import Category, Product, ProductImage, Brand
 from store.serializers import CategorySerializer, ProductSerializer, \
@@ -12,6 +13,8 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
 
     def destroy(self, request, *args, **kwargs):
         if Product.objects.select_related('category').filter(category_id=kwargs['pk']):
@@ -25,6 +28,8 @@ class ProductViewSet(viewsets.ModelViewSet):
         select_related('category').\
         select_related('brand').all()
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'brand__name', 'category__title']
 
     def get_serializer_class(self):
         if self.request.method in SAFE_METHODS:
@@ -38,6 +43,8 @@ class BrandViewSet(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
     def destroy(self, request, *args, **kwargs):
         if Product.objects.select_related('brand').filter(brand_id=kwargs['pk']):
