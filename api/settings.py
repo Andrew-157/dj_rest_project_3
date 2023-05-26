61be6b69d28bc14e3567467862ee9cefcc8ede74
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-26 09:52:46 +0300

    Installed django_filters

--- api/settings.py
+++ api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_cleanup.apps.CleanupConfig',
+    'django_filters',
     'rest_framework',
     'store'
 ]
@@ -143,5 +144,6 @@ REST_FRAMEWORK = {
     #     'rest_framework_simplejwt.authentication.JWTAuthentication',
     # ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
