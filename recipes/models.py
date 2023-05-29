61e9298de97bc6f620090b05f88f9ea2d631d51f
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-29 12:11:16 +0300

    Minor changes in Category model

--- recipes/models.py
+++ recipes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=155, unique=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=155, unique=True)
 
     def __str__(self):
         return self.title
