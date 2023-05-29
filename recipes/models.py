062a6ff5e2aef0f4d0e600116379fe051b7a779e
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-29 12:09:31 +0300

    Created model Category

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=155, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
