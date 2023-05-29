0836231a2754eb313d88551b2bbc73fa70e5646a
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-29 13:42:27 +0300

    Created Ingredient and Recipe models

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,58 @@
+from typing import Iterable, Optional
 from django.db import models
+from django.template.defaultfilters import slugify
 from users.models import CustomUser
 
 
 class Category(models.Model):
     title = models.CharField(max_length=155, unique=True)
     slug = models.SlugField(max_length=155, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Ingredient(models.Model):
+    MILLILITRES = 'ml'
+    MILLIGRAMS = 'mg'
+    OUNCES = 'oz'
+    LITRES = 'l'
+    GRAMS = 'gm'
+    UNITS_OF_MEASUREMENT = [
+        (MILLILITRES, 'millilitres'),
+        (MILLIGRAMS, 'milligrams'),
+        (OUNCES, 'ounces'),
+        (LITRES, 'litres'),
+        (GRAMS, 'grams')
+    ]
+    name = models.CharField(max_length=155)
+    quantity = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+    units_of_measurement = models.CharField(max_length=2,
+                                            choices=UNITS_OF_MEASUREMENT, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=155, unique=True)
+    slug = models.SlugField(max_length=155, unique=True, blank=True)
+    category = models.ForeignKey(Ingredient,
+                                 on_delete=models.PROTECT, related_name='recipes')
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Recipe, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
