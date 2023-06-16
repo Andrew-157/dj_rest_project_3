a6de564773bb729d8d604a2e531e234b01e97be7
Andrew Subotin <subotinandrey5@gmail.com>
2023-06-16 13:14:53 +0300

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -7,7 +7,14 @@ from users.models import CustomUser
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = [
         'username', 'email', 'date_joined',
-        'is_superuser', 'is_active', 'is_staff',
+        'is_superuser', 'is_active', 'is_staff', 'image_tag'
     ]
     list_filter = ['username', 'date_joined']
     search_fields = ['username']
+    readonly_fields = ['image_tag']
+
+    def image_tag(self, obj):
+        if obj.is_superuser:
+            return None
+        return format_html(f'<img src="{obj.image.url}" width="100" height="100">')
+    image_tag.short_description = "User's image"
